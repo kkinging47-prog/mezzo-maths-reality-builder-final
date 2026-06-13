6770901993104e93958bbb756ba0e436002904f8
kkinging47-prog <kkinging47@gmail.com>
2026-06-13 18:29:22 +0000

    Load student shell stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './motion.css';
+import './student-shell.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
