69be85ee27ef54249e06030d87d5f7c31bd9e5ce
kkinging47-prog <kkinging47@gmail.com>
2026-06-13 22:28:51 +0000

    Import Figma homepage styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './styles.css';
 import './motion.css';
 import './student-shell.css';
+import './landing.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
