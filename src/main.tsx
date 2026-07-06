970868ccf909f5ce053843b1b2c1723c6c654221
kkinging47-prog <kkinging47@gmail.com>
2026-07-06 08:33:26 +0000

    Load Babylon footbridge lab styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './motion.css';
 import './student-shell.css';
 import './landing.css';
 import './figma-mission.css';
+import './babylon-footbridge-lab.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
